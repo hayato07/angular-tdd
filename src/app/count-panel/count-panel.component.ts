6c6f0868d36a9e2d11a138b1e74fb6384153f6ff
Hayato Ando <0du38604360418k@gmail.com>
2024-03-04 12:10:20 +0900

    REFACTOR: ADD count panel component

---
+++ src/app/count-panel/count-panel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-count-panel',
+  standalone: true,
+  imports: [],
+  templateUrl: './count-panel.component.html',
+  styleUrl: './count-panel.component.scss'
+})
+export class CountPanelComponent {
+
+}
