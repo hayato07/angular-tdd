adb3bbef2dbcc34c75b14b31ef767c795e30a9be
Hayato Ando <0du38604360418k@gmail.com>
2024-03-04 12:12:40 +0900

    REFACTOR: FIX Count panel component

--- src/app/count-panel/count-panel.component.ts
+++ src/app/count-panel/count-panel.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-count-panel',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './count-panel.component.scss'
 })
 export class CountPanelComponent {
-
+  @Input() count = 0;
 }
