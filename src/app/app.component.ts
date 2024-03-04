cfc17586c98d71ba4eed4d12acbb061f2c660de5
Hayato Ando <0du38604360418k@gmail.com>
2024-03-04 19:25:15 +0900

    REFACTOR: add plus feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CountPanelComponent } from './count-panel/count-panel.component';
+import { CountButtonComponent } from './count-button/count-button.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CountPanelComponent],
+  imports: [RouterOutlet, CountPanelComponent, CountButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/count-button/count-button.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-count-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './count-button.component.html',
+  styleUrl: './count-button.component.scss'
+})
+export class CountButtonComponent {
+}
